95311d68a4a5c0989963e3a1d17edd450a2cf295
Anju Bishnoi <dishabishnoi93@gmail.com>
2025-07-06 15:47:49 +0530

    Update terraform2.tf
    made changes in terraform2.tf file

--- terraform-project/terraform2.tf
+++ terraform-project/terraform2.tf
@@ -1,4 +1,4 @@
-/* terraform {
+terraform {
   backend "s3" {
     bucket         = "terraform-backend-bucket-b"
     key            = "terraform-projects/terraform.tfstate" # <--- unique key
@@ -6,4 +6,4 @@
     dynamodb_table = "terraform-backend-table-d"
   }
 }
-*/
+
